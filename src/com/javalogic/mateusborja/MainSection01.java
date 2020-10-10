cd01b7656d85775ab77ae5b54c480d0bccceb916
mateus.borja <mateus.borja@gmail.com>
2020-10-10 19:08:17 -0400

    initial files

---
+++ src/com/javalogic/mateusborja/MainSection01.java
@@ -0,0 +1,21 @@
+
+package com.javalogic.mateusborja;
+
+public class MainSection01 {
+	public static void main(String[] args) {
+		Section01 s1 = new Section01();
+		
+		s1.sayName();
+		
+		s1.name = "Mateus Borja";
+		
+		System.out.println(s1.sayName());
+		
+		//s1.name = "Mateus Borja";
+		//System.out.println(s1.name);
+		
+		
+		
+	}
+
+}
---
+++ src/com/javalogic/mateusborja/Section01.java
@@ -0,0 +1,16 @@
+package com.javalogic.mateusborja;
+
+public class Section01 {
+	
+	String name;
+	
+	public String sayName() {
+		return name;
+		
+	}
+
+
+
+}
+	
+
