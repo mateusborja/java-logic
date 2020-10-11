5a90bebdf2d6568d24a2593049d23f48d42ba9a4
mateus.borja <mateus.borja@gmail.com>
2020-10-11 19:34:26 -0400

    start with methods and inheritance

--- src/com/mateusborja/javalogic/introducao/MainSection01.java
+++ src/com/mateusborja/javalogic/introducao/MainSection01.java
@@ -3,19 +3,22 @@ package com.mateusborja.javalogic.introducao;
 
 public class MainSection01 {
 	public static void main(String[] args) {
-		Section01 s1 = new Section01();
 		
-		s1.sayName();
+		Section01 n1 = new Section01("Joao Fernado", 25, 1.80, 80.4);
 		
-		s1.name = "Mateus Borja";
+
 		
-		System.out.println(s1.sayName());
-		
-		//s1.name = "Mateus Borja";
-		//System.out.println(s1.name);
+		n1.setName("Joao Ferdando Oliveira");
+		n1.setHeight(1.80);
+		n1.setWeight(80.5);
 		
 		
 		
+		System.out.println(n1.getName()  + "\n" + n1.getAge());
+
+		// s1.name = "Mateus Borja";
+		// System.out.println(s1.name);
+
 	}
 
 }
--- src/com/mateusborja/javalogic/introducao/Section01.java
+++ src/com/mateusborja/javalogic/introducao/Section01.java
@@ -1,16 +1,65 @@
 package com.mateusborja.javalogic.introducao;
 
 public class Section01 {
+
+	private String name;
+	private int age;
+	private double height;
+	private double weight;
+
+	public Section01(String name, int age, double height, double weight) {
+		this.name = name;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+
+	}
 	
-	String name;
-	
-	public String sayName() {
+
+	public String getName() {
 		return name;
-		
+	}
+
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
 	}
 
 
+	public double getHeight() {
+		return height;
+	}
+
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+
+	public Section01() {
+		
+	}
+	
+	
+	public String toString() {
+		return super.toString()+ "Seu noeme é: =>" + name + "Sua altura é: =>" + height + "E seu peso é: =>" + weight;
+	}
 
 }
-	
-
