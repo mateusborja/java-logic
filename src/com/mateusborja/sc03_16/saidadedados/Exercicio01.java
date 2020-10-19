af68d181723d0f2d29af8c0acd5be2c87e0280ea
mateus.borja <mateus.borja@gmail.com>
2020-10-18 22:25:08 -0400

    example

--- src/com/mateusborja/sc03_16/saidadedados/Exercicio01.java
+++ src/com/mateusborja/sc03_16/saidadedados/Exercicio01.java
@@ -21,11 +21,11 @@ public class Exercicio01 {
 
 		System.out.println("Products:");
 		System.out.printf("%s, wich price is $ %.2f%n", product1, price1);
-		System.out.printf("%s, wich price is $ %.2f%n", product2, price2);
-		System.out.printf("Records: %d years old, code %d and gender: %s%n", age, code, gender);
+		System.out.printf("%s, wich price is $ %.2f%n%n", product2, price2);
+		System.out.printf("Records: %d years old, code %d and gender: %s%n%n", age, code, gender);
 		System.out.printf("Measure with eight decimal places: %.8f%n", measure);
 		System.out.printf("Rouded (three decimal places): %.3f%n", measure);
-		Locale.setDefault(Locale.US);
+		Locale.setDefault(Locale.US); // classe que define o padrao de notação do aplicativo
 		System.out.printf("US decimal point: %.3f%n", measure);
 
 	}
