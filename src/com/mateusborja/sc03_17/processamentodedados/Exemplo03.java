1348af6c318df1cc9691b4a98a72c8e226d3b220
mateus.borja <mateus.borja@gmail.com>
2020-10-18 22:34:56 -0400

    examples

---
+++ src/com/mateusborja/sc03_17/processamentodedados/Exemplo03.java
@@ -0,0 +1,19 @@
+package com.mateusborja.sc03_17.processamentodedados;
+
+public class Exemplo03 {
+
+	public static void main(String[] args) {
+
+		double b, B, h, area;
+
+		b = 6.0;
+		B = 8.0;
+		h = 5.0;
+
+		area = ((b + B) / 2) * 5; // area = (b + B) / 2 * 5;
+
+		System.out.println(area);
+
+	}
+
+}
