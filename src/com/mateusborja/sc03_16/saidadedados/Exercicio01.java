da087ea50255dad453c6105c3f999094a8b75bf8
mateus.borja <mateus.borja@gmail.com>
2020-10-18 22:15:28 -0400

    example java type and interpolation printf

---
+++ src/com/mateusborja/sc03_16/saidadedados/Exercicio01.java
@@ -0,0 +1,26 @@
+//java output type
+package com.mateusborja.sc03_16.saidadedados;
+
+public class Exercicio01 {
+	
+	public static void main(String[] args) {
+		
+		String product1 = "Computer";
+		String product2 = "Office desk";
+		
+		int age = 30;
+		int code = 5290;
+		char gender = 'F';
+		
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		
+		double measure = 53.234567;
+		
+	
+		
+		
+		
+	}
+
+}
--- src/com/mateusborja/sc03_16/saidadedados/Main.java
+++ src/com/mateusborja/sc03_16/saidadedados/Main.java
@@ -1,5 +1,7 @@
 package com.mateusborja.sc03_16.saidadedados;
 
+import java.util.Locale;
+
 public class Main {
 	
 	public static void main(String[] args) {
@@ -9,12 +11,24 @@ public class Main {
 		
 		
 		int y = 32;
-		System.out.println(y);
+		
 		
 		double x = 10.35784;
+		
+		System.out.println(y);
 		System.out.println(x); 
-		System.out.printf("%.2f%n", x); //printf - formata com duas casas decimais "%.2f%n", mais a variavel
+		System.out.printf("%.2f%n", x); //printf - formata com duas casas decimais "%.2f%n", mais a variavel (printf pega o formato do padrao utilizado pelo computador)
 		System.out.printf("%.4f%n", x); //printf - formata com quatro casas decimais "%.4f%n", mais a variavel
+		Locale.setDefault(Locale.US); // define a localizacao
+		System.out.printf("%.4f%n", x); //printf - formata com quatro casas decimais "%.4f%n", mais a variavel
+		System.out.println("RESULTADO = " + x + " METROS."); //println
+		System.out.printf("RESULTADO => %.2f metros%n", x); // contatenando com printf => %.2f e substituido pela variavel x
+		
+		String nome = "Maria";
+		int idade = 31;
+		double renda = 4000;
+		
+		System.out.printf("%s tem %d anos e ganha R$ %.2f reais %n", nome, idade, renda); //%s texto, %d inteiro, %f ponto flutuate, %n quebra de linha (valores interpolados)
 		
 		
 		
