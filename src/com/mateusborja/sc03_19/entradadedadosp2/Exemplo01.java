9c0c96e1fc4db1bc8ad2a5060462878e0979b5a3
mateus.borja <mateus.borja@gmail.com>
2020-10-18 23:09:00 -0400

    java input type

---
+++ src/com/mateusborja/sc03_19/entradadedadosp2/Exemplo01.java
@@ -0,0 +1,25 @@
+//java Scanner type
+package com.mateusborja.sc03_19.entradadedadosp2;
+
+import java.util.Scanner;
+
+public class Exemplo01 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		String s1, s2, s3;
+
+		s1 = sc.nextLine();
+		s2 = sc.nextLine();
+		s3 = sc.nextLine();
+
+		System.out.println("Dados digitados: ");
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+
+	}
+
+}
