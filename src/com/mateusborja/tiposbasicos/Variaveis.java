120a55a41e59e7d72d5efe7bd62279ead1052d77
mateus.borja <mateus.borja@gmail.com>
2020-10-18 21:32:26 -0400

    java types

---
+++ src/com/mateusborja/tiposbasicos/Variaveis.java
@@ -0,0 +1,16 @@
+package com.mateusborja.tiposbasicos;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+
+		int idade = 25;
+		double altura = 1.68;
+		char sexo = 'F';
+		char a = '\u0061';
+		
+		System.out.println(idade + " " + altura + " " +  " " + " " + sexo + " " +  " " + a);
+
+	}
+
+}
