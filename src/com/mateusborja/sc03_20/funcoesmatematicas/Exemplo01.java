29be419e2f1214ddc6c5d732dd3d4def280ed580
mateus.borja <mateus.borja@gmail.com>
2020-10-18 23:27:02 -0400

    java functions Math.sqrt, pow and abs

---
+++ src/com/mateusborja/sc03_20/funcoesmatematicas/Exemplo01.java
@@ -0,0 +1,14 @@
+//java functions
+package com.mateusborja.sc03_20.funcoesmatematicas;
+
+public class Exemplo01 {
+	
+	public static void main(String[] args) {
+		
+		double x = 3.0;
+		double y = 4.0;
+		
+		
+	}
+
+}
