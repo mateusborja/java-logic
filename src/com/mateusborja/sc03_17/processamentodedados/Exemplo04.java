5e305fdce58f0737a6bea62cebbb745e117a1f2f
mateus.borja <mateus.borja@gmail.com>
2020-10-18 22:44:15 -0400

    java casting

---
+++ src/com/mateusborja/sc03_17/processamentodedados/Exemplo04.java
@@ -0,0 +1,20 @@
+//java examples with casting
+package com.mateusborja.sc03_17.processamentodedados;
+
+public class Exemplo04 {
+
+	public static void main(String[] args) {
+
+		int a, b;
+		double resultado;
+
+		a = 500;
+		b = 200;
+
+		resultado = (double) a / b;
+
+		System.out.println(resultado);
+
+	}
+
+}
---
+++ src/com/mateusborja/sc03_17/processamentodedados/Exemplo05.java
@@ -0,0 +1,28 @@
+//java casting
+package com.mateusborja.sc03_17.processamentodedados;
+
+public class Exemplo05 {
+
+	public static void main(String[] args) {
+
+		double a;
+		int b;
+
+		double c;
+		int d;
+
+		a = 5.0;
+		b = (int) a;
+
+		c = 4;
+		d = (int) c; //casting
+
+		System.out.println(a);
+		System.out.println(b);
+
+		System.out.println(c);
+		System.out.println(d);
+
+	}
+
+}
