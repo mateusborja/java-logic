5016dc660f10601b6a62dcee85dc2bd2ca4b97a0
mateus.borja <mateus.borja@gmail.com>
2020-10-18 22:34:18 -0400

    examples

---
+++ src/com/mateusborja/sc03_17/processamentodedados/Exemplo01.java
@@ -0,0 +1,20 @@
+//java examples
+package com.mateusborja.sc03_17.processamentodedados;
+
+public class Exemplo01 {
+	
+	public static void main(String[] args) {
+		
+		int x, y;
+		
+		x = 5;
+		
+		y = 2 * x;
+		
+		System.out.println(x);
+		System.out.println(y);
+		
+		
+	}
+
+}
