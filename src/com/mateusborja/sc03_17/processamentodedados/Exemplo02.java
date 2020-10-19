c19bdd3f260bbed55bc53ab688696c61c54b862e
mateus.borja <mateus.borja@gmail.com>
2020-10-18 22:34:43 -0400

    examples

---
+++ src/com/mateusborja/sc03_17/processamentodedados/Exemplo02.java
@@ -0,0 +1,18 @@
+package com.mateusborja.sc03_17.processamentodedados;
+
+public class Exemplo02 {
+
+	public static void main(String[] args) {
+
+		int x;
+		double y;
+
+		x = 5;
+		y = 2 * x;
+
+		System.out.println(x);
+		System.out.println(y); // variavel do tipo ponto flutuante
+
+	}
+
+}
