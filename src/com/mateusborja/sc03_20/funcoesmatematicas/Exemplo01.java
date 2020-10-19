fee8b0ebce8296ecd130dd6ad3a7294602b0214e
mateus.borja <mateus.borja@gmail.com>
2020-10-18 23:49:32 -0400

    examples java functions Math

--- src/com/mateusborja/sc03_20/funcoesmatematicas/Exemplo01.java
+++ src/com/mateusborja/sc03_20/funcoesmatematicas/Exemplo01.java
@@ -2,11 +2,36 @@
 package com.mateusborja.sc03_20.funcoesmatematicas;
 
 public class Exemplo01 {
-	
+
 	public static void main(String[] args) {
-		
+
 		double x = 3.0;
-		double y = 4.0;
+		double y = 4;
+		double z = -5.0;
+
+		double A, B, C;
+
+		A = Math.sqrt(x);
+		B = Math.sqrt(y);
+		C = Math.sqrt(25);
+
+		System.out.printf("Raiz quadrada é: => %.4f%n", A);
+		System.out.println("Raiz quadrada é: => " + B);
+		System.out.println("Raiz quadrada é: => " + C + "\n");
+
+		A = Math.pow(x, y);
+		B = Math.pow(x, 2.0);
+		C = Math.pow(5.0, 2.0);
+
+		System.out.println(x + " elevado a: => " + y + " é igual a: " + A);
+		System.out.println(x + " elevado ao quadrado é igual a: => " + B);
+		System.out.println("5 elevado ao quadrado é igual a: => " + C + "\n");
+		
+		A = Math.abs(x);
+		B = Math.abs(z); // valor positivo
+		System.out.println("Valor absoluto é: => " + A);
+		System.out.println("Valor absoluto é: => " + B);
+		
 		
 		
 	}
---
+++ src/com/mateusborja/sc03_20/funcoesmatematicas/Exemplo02.java
@@ -0,0 +1,27 @@
+//java functions and example
+package com.mateusborja.sc03_20.funcoesmatematicas;
+
+public class Exemplo02 {
+	
+	public static void main(String[] args) {
+		
+		double a, b, c, delta, x1, x2;
+		
+		a = 15; 
+		b = -25;
+		c = 38;
+		
+		//formula de bascara
+		delta = Math.pow(b, 2.0) - 4 * a * c;
+
+		x1 = (-b + Math.sqrt(delta)) / (2.0 * a);
+		x2 = (-b - Math.sqrt(delta)) / (2.0 * a);
+		
+		System.out.printf("%.2f%n%.2f%n%.2f%n", delta, x1, x2);
+		
+		
+		
+		
+	}
+
+}
