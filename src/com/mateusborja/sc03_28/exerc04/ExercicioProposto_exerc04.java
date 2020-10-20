59d283c4fec094be033cb2d07e774b3658c7b8c3
mateus.borja <mateus.borja@gmail.com>
2020-10-20 17:22:03 -0400

    uri 1019

---
+++ src/com/mateusborja/sc03_28/exerc04/ExercicioProposto_exerc04.java
@@ -0,0 +1,29 @@
+/* java 
+ * uri online 1019
+ */
+package com.mateusborja.sc03_28.exerc04;
+
+import java.util.Scanner;
+
+public class ExercicioProposto_exerc04 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int N, resto, horas, minutos, segundos;
+
+		N = sc.nextInt();
+
+		horas = N / 3600;
+		resto = N % 3600;
+
+		minutos = resto / 60; // 60min
+
+		segundos = resto % 60;
+
+		System.out.println(horas + ":" + minutos + ":" + segundos);
+
+	}
+
+}
