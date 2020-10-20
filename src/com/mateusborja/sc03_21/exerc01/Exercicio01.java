16a10b7b62c36a92631c9e74878a998d76a8886c
mateus.borja <mateus.borja@gmail.com>
2020-10-19 21:55:12 -0400

    java example

---
+++ src/com/mateusborja/sc03_21/exerc01/Exercicio01.java
@@ -0,0 +1,38 @@
+//java exercicio01
+package com.mateusborja.sc03_21.exerc01;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio01 {
+	
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US); // set default US input  type
+		Scanner sc = new Scanner(System.in);
+		
+		double largura, comprimento, metroQuadrado;
+		double area;
+		double precoTotal;
+		
+		System.out.println("Digite o valor da Largura:");
+		largura = sc.nextDouble();
+		System.out.println("Digite o valor do Comprimento:");
+		comprimento = sc.nextDouble();
+		System.out.println("Digite o valor do metro quadrado:");
+		metroQuadrado = sc.nextDouble();
+
+		area = largura * comprimento;
+		precoTotal = area * metroQuadrado;
+		
+		System.out.println("A area do terreno é: " + area);
+		System.out.println("Preço do terreno é: " + precoTotal);
+		
+		sc.close();
+		
+		
+		
+		
+	}
+
+}
