9f113c7f92c36f14c41ef5ea4e0c2a14e405cf98
mateus.borja <mateus.borja@gmail.com>
2020-10-20 10:24:27 -0400

    uri 1011

--- src/com/mateusborja/sc03_23/exercURI/ExercicioUriOnline1011.java
+++ src/com/mateusborja/sc03_23/exercURI/ExercicioUriOnline1011.java
@@ -5,23 +5,20 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class ExercicioUriOnline1011 {
-	
+
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		
+
 		double pi = 3.14159, raio, volumeEsfera;
-		
+
 		raio = sc.nextDouble();
-		
-		volumeEsfera = (4/3.0) * pi * raio * raio * raio;
-		
+
+		volumeEsfera = (4 / 3.0) * pi * raio * raio * raio; //formula calculo esfera
+
 		System.out.printf("VOLUME = %.3f%n", volumeEsfera);
-		
-		
-		
+
 	}
 
 }
