2623e056f12a8661f8317f4e18cda24ad9e596a1
mateus.borja <mateus.borja@gmail.com>
2020-10-19 23:01:22 -0400

    URI online example

---
+++ src/com/mateusborja/sc03_23/exercURI/ExercicioUriOnline1001.java
@@ -0,0 +1,33 @@
+//java URI online 
+package com.mateusborja.sc03_23.exercURI;
+
+import java.util.Scanner;
+
+public class ExercicioUriOnline1001 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A, B, X;
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		
+		X = A + B;
+		
+		System.out.println("X = " + X);
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+		
+		
+	}
+	
+
+}
