adbe9ad86179b19dc8222ed72bac07fa81b05366
mateus.borja <mateus.borja@gmail.com>
2020-10-21 23:20:56 -0400

    java operators

---
+++ src/com/mateusborja/sc04_32/expressoescomparativas/ComparacaoUm.java
@@ -0,0 +1,38 @@
+//java expressions
+package com.mateusborja.sc04_32.expressoescomparativas;
+
+public class ComparacaoUm {
+
+	public static void main(String[] args) {
+
+		int x = 5;
+
+		if (x > 0) {
+			System.out.println("Resultado Verdadeiro" + " x > 0");
+
+		} else
+			System.out.println("Resultado Falso" + "x > 0");
+		
+		if (x == 3) {
+			System.out.println("Resultado Verdadeiro" + " x == 3");
+			
+		} else
+			System.out.println("Resultado Falso" + " x == 3");
+		
+		if (10 <= 30) {
+			System.out.println("Resultado Verdadeiro" + " 10 <= 30");
+			
+		} else
+			System.out.println("Resultado Falso" + " 10 <= 30");
+		
+		if (x != 2) {
+			System.out.println("Resultado Verdadeiro" + " x != 2");
+			
+		} else
+			System.out.println("Resultado Falso" + " x != 2");
+		
+
+
+	}
+
+}
