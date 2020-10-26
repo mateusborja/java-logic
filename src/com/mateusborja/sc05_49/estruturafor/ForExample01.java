d438f2e7e30c00caab626e46ed1ddad367da7be9
mateus.borja <mateus.borja@gmail.com>
2020-10-26 08:57:46 -0400

    java for

---
+++ src/com/mateusborja/sc05_49/estruturafor/ForExample01.java
@@ -0,0 +1,26 @@
+//java for
+package com.mateusborja.sc05_49.estruturafor;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class ForExample01 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int soma = 0;
+		int n = sc.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			int x = sc.nextInt();
+			soma += x;
+
+		}
+
+		System.out.println(soma);
+		sc.close();
+	}
+
+}
