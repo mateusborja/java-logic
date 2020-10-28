46fc6f79000486f56bfd3607c12ed5eef74712a1
mateus.borja <mateus.borja@gmail.com>
2020-10-27 22:58:28 -0400

    for example

---
+++ src/com/mateusborja/zsamples/ForExample01.java
@@ -0,0 +1,18 @@
+package com.mateusborja.zsamples;
+
+public class ForExample01 {
+
+	public static void main(String[] args) {
+
+		int x = 0;
+
+		int soma = 0;
+		for (int i = 0; i < 7; i++) {
+			x++;
+			soma += x;
+			System.out.println(soma);
+		}
+
+	}
+
+}
