cf93d0d83e655cec7d68c48e16770b4181e5d612
mateus.borja <mateus.borja@gmail.com>
2020-10-27 23:12:26 -0400

    numeros pares e impares

---
+++ src/com/mateusborja/zsamples/NumerosImpares.java
@@ -0,0 +1,16 @@
+package com.mateusborja.zsamples;
+
+public class NumerosImpares {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i <= 10; i++) {
+			if (i % 2 != 0) {
+				System.out.println("Numeros Impares de 0 a 10 = " + i);
+			}
+
+		}
+
+	}
+
+}
---
+++ src/com/mateusborja/zsamples/NumerosPares.java
@@ -0,0 +1,18 @@
+package com.mateusborja.zsamples;
+
+public class NumerosPares {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 10; i++) {
+
+			if (i % 2 == 0) { // numero par de zero a dez
+				System.out.println("Numero par: " + i);
+
+			} 
+
+		}
+
+	}
+
+}
