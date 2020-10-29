2b2ffbe683e0ea9f57b8e4118357542069c90c73
mateus.borja <mateus.borja@gmail.com>
2020-10-28 22:58:08 -0400

    uri 1071 soma numeros impares consecutivos

---
+++ src/com/mateusborja/sc05_52/exerURI/ExercicioUriOnline1078.java
@@ -0,0 +1,23 @@
+//java for uri 1078 - tabuada
+package com.mateusborja.sc05_52.exerURI;
+
+import java.util.Scanner;
+
+public class ExercicioUriOnline1078 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		for (int i = 1; i <= 10; i++) {
+			int produto = i * N;
+			System.out.println(i + " x " + N + " = " + produto);
+		}
+
+		sc.close();
+
+	}
+
+}
