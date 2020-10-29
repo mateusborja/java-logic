e15e5bd3922beaadb48ba4fe05d7531d9bde2455
mateus.borja <mateus.borja@gmail.com>
2020-10-29 09:35:54 -0400

    celsius to fahrenheit with java do while

---
+++ src/com/mateusborja/sc05_54/estruturadowhile/DoWhileExemplo01.java
@@ -0,0 +1,29 @@
+//java do while
+package com.mateusborja.sc05_54.estruturadowhile;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class DoWhileExemplo01 {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		char repetir;
+
+		do {
+
+			System.out.print("Digite a temperatura em Celsius: ");
+			double celsius = sc.nextDouble();
+			double fahrenheit = 9.0 * celsius / 5.0 + 32.0;
+			System.out.printf("Equivalente em Fahrenheit é: %.1f F%n", fahrenheit);
+			System.out.printf("Deseja repetir: (s/n):%n");
+			repetir = sc.next().charAt(0);
+
+		} while (repetir != 'n');
+
+	}
+
+}
