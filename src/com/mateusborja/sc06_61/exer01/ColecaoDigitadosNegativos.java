6337131905a2233111bf5633fef580d9dc30e0b7
mateus.borja <mateus.borja@gmail.com>
2020-10-29 14:14:22 -0400

    exemplo vetores numeros digitados negativos e positivos

---
+++ src/com/mateusborja/sc06_61/exer01/ColecaoDigitadosNegativos.java
@@ -0,0 +1,34 @@
+//java array numeros digitados de uma colecao
+package com.mateusborja.sc06_61.exer01;
+
+import java.util.Scanner;
+
+public class ColecaoDigitadosNegativos {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Quantidade de Vetores da Coleção:");
+		int n = sc.nextInt();
+
+		int[] vet = new int[n];
+
+		System.out.println("Digite os Vetores da Coleção:");
+
+		for (int i = 0; i < n; i++) {
+			vet[i] = sc.nextInt();
+
+		}
+
+		System.out.println("");
+		for (int i = 0; i < n; i++) {
+			if (vet[i] < 0) {
+				System.out.print(vet[i] + ", ");
+			}
+
+		}
+
+	}
+
+}
---
+++ src/com/mateusborja/sc06_61/exer01/ColecaoDigitadosPositivos.java
@@ -0,0 +1,32 @@
+//java array
+package com.mateusborja.sc06_61.exer01;
+
+import java.util.Scanner;
+
+public class ColecaoDigitadosPositivos {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Quantidade de Vetores na Coleção: ");
+		int n = sc.nextInt();
+
+		int[] vet = new int[n];
+
+		System.out.println("Números da Coleção: ");
+		for (int i = 0; i < n; i++) {
+			vet[i] = sc.nextInt();
+		}
+
+		for (int i = 0; i < n; i++) {
+			if (vet[i] > 0) {
+				System.out.println(vet[i]);
+
+			}
+
+		}
+
+	}
+
+}
