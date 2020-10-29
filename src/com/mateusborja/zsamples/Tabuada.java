2e73ba257f3025253b60e43693fc5f7ff24a92dc
mateus.borja <mateus.borja@gmail.com>
2020-10-29 08:29:47 -0400

    example tabuada

---
+++ src/com/mateusborja/zsamples/Tabuada.java
@@ -0,0 +1,24 @@
+//Tabuada
+package com.mateusborja.zsamples;
+
+import java.util.Scanner;
+
+public class Tabuada {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Tabuada do Número?");
+		int n = sc.nextInt();
+
+		System.out.println("");
+		for (int i = 1; i <= 10; i++) {
+			int tabuada = i * n;
+			System.out.println(i + " x " + n + " = " + tabuada);
+
+		}
+
+	}
+
+}
