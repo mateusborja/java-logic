59fc551464d26750aa35d7bb28005b45a5983a08
mateus.borja <mateus.borja@gmail.com>
2020-10-28 22:15:38 -0400

    java board test 06 java for

---
+++ src/com/mateusborja/sc05_50/testedemesa/TesteDeMesaFor07.java
@@ -0,0 +1,19 @@
+//java board test 06 java for
+package com.mateusborja.sc05_50.testedemesa;
+
+public class TesteDeMesaFor07 {
+
+	public static void main(String[] args) {
+
+		int x = 8, y = 3;
+
+		for (int i = 0; y < x; i++) {
+			x = x - 2;
+			y = y + 1;
+			System.out.println(i);
+
+		}
+
+	}
+
+}
