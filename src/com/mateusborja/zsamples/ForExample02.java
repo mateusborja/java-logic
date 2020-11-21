bf5f718b86632ccbab45a5edf820b05dacfdb8bd
Mateus Borja <mateus.borja@gmail.com>
2020-11-21 14:38:32 -0400

    exemplo de vetores

---
+++ src/com/mateusborja/zsamples/ForExample02.java
@@ -0,0 +1,19 @@
+//java super for
+package com.mateusborja.zsamples;
+
+public class ForExample02 {
+	
+	public static void main(String[] args) {
+		
+		
+		String[] nomes = {"Joao", "Maria", "Ledinha", "Ligia", "Iracema"};
+		
+		
+		for (String nome : nomes) { //valor do arry definido
+			System.out.print(nome + ", ");
+		}
+		
+		
+	}
+
+}
