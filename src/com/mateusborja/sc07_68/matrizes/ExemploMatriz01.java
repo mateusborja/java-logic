0c7e9ac3d1cac58ef582334ec30cc642991ce03a
Mateus Borja <mateus.borja@gmail.com>
2020-11-21 14:40:51 -0400

    exemplo de matrizes

--- src/com/mateusborja/sc07_68/matrizes/ExemploMatriz01.java
+++ src/com/mateusborja/sc07_68/matrizes/ExemploMatriz01.java
@@ -8,12 +8,14 @@ public class ExemploMatriz01 {
 
 		Scanner sc = new Scanner(System.in);
 
-		System.out.print("tell me rows and columns: ");
+		System.out.print("tell me rows: ");
 		int M = sc.nextInt();
+		System.out.print("tell me columns: ");
 		int N = sc.nextInt();
 
 		int[][] mat = new int[M][N];
 
+		System.out.println("tell me numbers: ");
 		for (int i = 0; i < M; i++) { // for que ira ler a matriz
 			for (int j = 0; j < N; j++) {
 				mat[i][j] = sc.nextInt();
